4cb7882368067120319907d8d3f0ec9d7c635b6b
Navneet Mittal <mittal.navneet@gmail.com>
2025-10-12 20:36:56 +0530

    updated uploadConfirmation.tsx

--- frontend/src/components/Upload/UploadConfirmation.tsx
+++ frontend/src/components/Upload/UploadConfirmation.tsx
@@ -30,7 +30,7 @@ export function UploadConfirmation({
 const formData = new FormData();
 formData.append("file", selectedFile);
 
-    fetch(`https://pdf-ocr-app-snowy.vercel.app/output/${encodeURIComponent(htmlFile)}`)
+    fetch(`http://pdf-ocr-app-snowy.vercel.app/output/${encodeURIComponent(htmlFile)}`)
       .then((res) => res.text())
       .then((rawHtml) => {
         let html = rawHtml;
